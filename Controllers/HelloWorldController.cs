ae2aff4cc6195d41a7cecc2d20d683caac574d75
al1na-am1rova <li.amirova2000@mail.ru>
2025-09-02 16:43:53 +0300

    метод Welcome включает параметры name и ID (Этап 2 завершен)

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -14,9 +14,9 @@ namespace Amirova.Controllers
         // 
         // GET: /HelloWorld/Welcome/ 
         // Requires using System.Text.Encodings.Web;
-        public string Welcome(string name, int numTimes = 1)
+        public string Welcome(string name, int ID = 1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
         }
     }
 }
