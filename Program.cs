8f353407b20c49d324ed58fb6c0224daa9b6978d
al1na-am1rova <li.amirova2000@mail.ru>
2025-09-05 15:55:11 +0300

    Заполнена БД(Этап 5 завершен)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Amirova.Data;
+using Amirova.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AmirovaContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AmirovaContext") ?? throw new InvalidOperationException("Connection string 'AmirovaContext' not found.")));
@@ -9,6 +10,13 @@ builder.Services.AddDbContext<AmirovaContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
